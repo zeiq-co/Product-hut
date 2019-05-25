a4f525bbc33c8e4ef2c926fa3d08e9e86141f02b
Sourav <sourav@Parminders-Mac-mini.local>
2019-05-25 21:27:53 +0530

    added card

--- src/components/elements/Cards.js
+++ src/components/elements/Cards.js
@@ -2,32 +2,42 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
-   .media-content {
-    padding: 9% 0% 10% 0%;
-   }
-   .title {
-    line-height: 2;
-   }
-   .card {
-       :hover {
-        box-shadow: 0 5px 15px rgba(37,37,37,.3);
-       }
-   }
+    background-color: ${props=>props.theme.white};
+    padding: 2% 2%;
+    margin: 2% 2%;
+    .title {
+        line-height: 2;
+    }
+    .icons {
+    margin-top: 2rem;
+    }
 `;
 const Cards = ({ image, title, subtitle }) => (
-    <Container className="">
-        <div className="card">
-            <div className="card-image">
-                <figure className="image is-1by1">
-                    <img src={image} alt="" />
-                </figure>
-            </div>
-            <div className="media-content has-text-centered	">
-                <p className="title is-size-5 has-text-weight-bold has-text-black	is-4">{title}</p>
-                <p className="subtitle is-family-monospace	is-6">{subtitle}</p>
-            </div>
+  <Container className="">
+    <article className="media">
+      <figure className="media-left">
+        <p className="image is-128x128">
+          <img src={image} alt="" />
+        </p>
+      </figure>
+      <div className="media-content">
+        <div className="content">
+          <p className="title is-size-5 has-text-weight-bold has-text-black	is-4">{title}</p>
+          <p className="subtitle is-family-monospace	is-6">{subtitle}</p>
         </div>
-    </Container>
+        <nav className="level is-mobile">
+          <div className="level-left">
+            <a className="level-item">
+              <span className="icon is-small"><i className="fas fa-comment-dots" /></span>
+            </a>
+          </div>
+        </nav>
+      </div>
+      <div className="media-right">
+        <span className="icons is-small is-size-2"><i className="icons fas fa-chevron-right" /></span>
+      </div>
+    </article>
+  </Container>
 );
 
 export default Cards;
--- src/components/firstSection.js
+++ src/components/firstSection.js
@@ -1,21 +1,45 @@
 import React from 'react';
 
 import styled from 'styled-components';
+import Cards from './elements/Cards';
 
 const Container = styled.div`
   background-color: ${props => props.theme.backgroundColor};
 `;
 
+const FirstImage ='../images/red.jpeg';
+
 export default class FirstSection extends React.Component {
   render() {
     return (
       <Container>
-        <div className="columns">
-          <div className="column is-three-fifths">
-            First column
-          </div>
-          <div className="column">
+        <div className="container">
+          <div className="columns">
+            <div className="column is-three-fifths">
+            
+              <Cards
+                image={FirstImage}
+                title="But wait, why the API edition?"
+                subtitle="Over the years, Makers have delighted us and the wider community with products built on our API.
+                 We’ve seen Twitter bots, to data analysis to social networks."
+              />
+
+              <Cards
+                image={FirstImage}
+                title="But wait, why the API edition?"
+                subtitle="Over the years, Makers have delighted us and the wider community with products built on our API.
+                 We’ve seen Twitter bots, to data analysis to social networks."
+              />
+              <Cards
+                image={FirstImage}
+                title="But wait, why the API edition?"
+                subtitle="Over the years, Makers have delighted us and the wider community with products built on our API.
+                 We’ve seen Twitter bots, to data analysis to social networks."
+              />
+            </div>
+            <div className="column">
             Second column
+            </div>
           </div>
         </div>
       </Container>
